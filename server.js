1c4b7c4a8cc67ecd1cc87238179768f5569b5b10
jazmin10 <jazminestrada10@gmail.com>
2018-07-12 14:00:36 -0500

    starting express server

---
+++ server.js
@@ -0,0 +1,19 @@
+// =========== START THE SERVER ===========
+
+	// Import dependencies
+	var express = require("express");
+	var bodyParser = require("body-parser");
+
+	var PORT = 3000;
+	var app = express();
+
+	// Middleware
+	app.use(bodyParser.json());
+	app.use(bodyParser.urlencoded({extended: true}));
+	app.use(bodyParser.text());
+	app.use(bodyParser.json({type: "application/vnd.api+json"}));
+
+	// Start the server
+	app.listen(PORT, function() {
+		console.log("You are listening on PORT:", PORT);
+	});
